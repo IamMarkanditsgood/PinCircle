53dcc0f7daa2697cbc903860d11a02c8ac356585
PavloMarkovskiy <IamMarkanditsgood@users.noreply.github.com>
2025-03-13 09:46:52 +0000

    Fix

--- Assets/Scripts/Scripts/UI/Popups/Variables/Lose.cs
+++ Assets/Scripts/Scripts/UI/Popups/Variables/Lose.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Lose : BasicPopup
@@ -9,17 +10,20 @@ public class Lose : BasicPopup
     [SerializeField] private TMP_Text _score;
 
     [SerializeField] private Button _playAgain;
+    [SerializeField] private Button _home;
 
     [SerializeField] private Orbit_Manager _orbitManager;
 
     private void Start()
     {
         _playAgain.onClick.AddListener(StartAgain);
+        _home.onClick.AddListener(Home);
     }
 
     private void OnDestroy()
     {
         _playAgain.onClick.RemoveListener(StartAgain);
+        _home.onClick.RemoveListener(Home);
     }
     public override void ResetPopup()
     {
@@ -40,5 +44,10 @@ public class Lose : BasicPopup
         _orbitManager.enabled = true;     
         UIManager.Instance.HidePopup(PopupType);
         UIManager.Instance.ShowScreen(ScreenTypes.Game);
+    }
+
+    private void Home()
+    {
+        SceneManager.LoadScene("Game");
     }
 }
